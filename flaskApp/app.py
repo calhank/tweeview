46856b408deca08e983b870d78aa36a01c6f00d1
calhank <hank.m@berkeley.edu>
2016-04-19 20:39:01 -0400

    added filterable word cloud

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -40,36 +40,55 @@ def parse_tweet_array(tweet_array):
 	output["TAG_COUNT"] = Counter()
 	output["TAG_COUNT_NON_RT"] = Counter()
 	output["GLOBAL_SENTIMENT"] = []
+	output["WORD_COUNT"] = Counter()
+	output["WORD_COUNT_NON_RT"] = Counter()
+	all_hashtags_processed = set()
 	
 	for tweet_tuple in tweet_array:
 		try:
 			tweet = tweet_tuple[1]
+			text_list = tweet["text"].split(" ")
 			ht = tweet["hashtag"]
+			all_hashtags_processed.add(ht)
 			output["TAG_COUNT"][ht] += 1
-			if tweet["is_rt"]:
+			output["WORD_COUNT"].update(text_list)
+			if not tweet["is_rt"]:
 				output["TOTAL_TWEET_COUNT_NON_RT"] += 1
 				output["TAG_COUNT_NON_RT"][ht] += 1
+				output["WORD_COUNT_NON_RT"].update(text_list)
 
 			sentimentTuple = (tweet["timestamp"], tweet["sentiment"], tweet["is_rt"])
 			output["GLOBAL_SENTIMENT"].append( sentimentTuple )
 			try:
 				output[ht]["sentiment_series"].append( sentimentTuple )
 				output[ht]["total_observations"] += 1
-				for word in tweet["text"].split(" "):
-					output[ht]["word_count"][word] += 1
+				output[ht]["word_count"].update(text_list)
+				output[ht]["related_hashtags"].update(tweet["related_hashtags"])
+				output[ht]["related_links"].update(tweet["related_links"])
 
 			except KeyError:
 				output[ht] = {}
 				output[ht]["sentiment_series"] = [ sentimentTuple ]
 				output[ht]["total_observations"] = 1
 				output[ht]["word_count"] = Counter()
-				for word in tweet["text"].split(" "):
-					output[ht]["word_count"][word] += 1
+				output[ht]["word_count"].update(text_list)
+				output[ht]["related_hashtags"] = Counter()
+				output[ht]["related_hashtags"].update(tweet["related_hashtags"])
+				output[ht]["related_links"] = Counter()
+				output[ht]["related_links"].update(tweet["related_links"])
 
 		except Exception,e:
 			print e
 
+	for ht in all_hashtags_processed:
+		output[ht]["word_count"] = output[ht]["word_count"].most_common()
+		output[ht]["related_hashtags"] = output[ht]["related_hashtags"].most_common()
+		output[ht]["related_links"] = output[ht]["related_links"].most_common()
+
 	output["TOP_TAGS"] = output["TAG_COUNT"].most_common()
+	output["TOP_TAGS_NON_RT"] = output["TAG_COUNT_NON_RT"].most_common()
+	output["TOP_WORDS"] = output["WORD_COUNT"].most_common(200)
+	output["TOP_WORDS_NON_RT"] = output["WORD_COUNT_NON_RT"].most_common(200)
 	output["TOTAL_TAG_COUNT"] = len(output["TAG_COUNT"])
 	output["TOTAL_TAG_COUNT_NON_RT"] = len(output["TAG_COUNT_NON_RT"])
 
@@ -101,12 +120,15 @@ class TweeviewListener(tweepy.StreamListener):
 			parsed_tweet["timestamp"] = mktime(status.created_at.timetuple())
 			parsed_tweet["sentiment"] = sentiment(status.text.encode('utf-8'))["compound"]
 			parsed_tweet["text"] = status.text.encode('utf-8')
+			parsed_tweet["related_links"] = [ lnk["url"] + "||" + lnk["display_url"] for lnk in status.entities["urls"]]
 
 			if len(status.entities["hashtags"]) == 0:
+				parsed_tweet["related_hashtags"] = []
 				parsed_tweet["hashtag"] = "(No Hashtag)"
 				tweet_array.append( (now, copy.deepcopy(parsed_tweet)) )
 			else:
 				for ht in status.entities["hashtags"]:
+					parsed_tweet["related_hashtags"] = [ lnk["text"] for lnk in status.entities["hashtags"] if lnk["text"] != ht["text"] ]
 					parsed_tweet["hashtag"] = ht["text"]
 					tweet_array.append( (now, copy.deepcopy(parsed_tweet)) )
 		
